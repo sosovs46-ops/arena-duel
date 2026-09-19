dbc41384b76e158980fb624c2e80152847605be9
sosovs46-ops <sosovs46@gmail.com>
2026-09-19 19:54:17 +0300

    Configure Firebase web app credentials for Arena Duel

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
 window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "0000000000",
-  appId: "1:0000000000:web:xxxxxxxxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyAe0cghgeavob_Ex6cz73jcvxIAutEpwo",
+  authDomain: "arena-duel-1f0d9.firebaseapp.com",
+  databaseURL: "https://arena-duel-1f0d9-default-rtdb.firebaseio.com",
+  projectId: "arena-duel-1f0d9",
+  storageBucket: "arena-duel-1f0d9.firebasestorage.app",
+  messagingSenderId: "1086562699065",
+  appId: "1:1086562699065:web:477d45775d728d42cb4ae8",
+  measurementId: "G-4HSLPvCT02"
 };

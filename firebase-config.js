42d19525feef8c20ac27254b82a21411962ec0eb
sosovs46-ops <sosovs46@gmail.com>
2026-09-19 19:08:30 +0300

    Add Firebase config template for public deployment

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "0000000000",
+  appId: "1:0000000000:web:xxxxxxxxxxxxxxxxxxxxxxxx"
+};
